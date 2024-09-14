5431e7b080b86829270a71a61acf702c39d039fb
Padraic Shafer <pshafer@bnl.gov>
2024-09-14 08:40:40 -0700

    Add tests for optional TiledDataSource methods

--- PyMca5/tests/ut/test_tiled_data_source.py
+++ PyMca5/tests/ut/test_tiled_data_source.py
@@ -1,4 +1,5 @@
 import enable_pymca_import  # noqa: F401
+import pytest
 
 from PyMca5.PyMcaCore.TiledDataSource import TiledDataSource
 
@@ -7,8 +8,51 @@ def test_init():
     """Can create a TiledDataSource object."""
     TiledDataSource()
 
+###############################################################################
+# OPTIONAL METHODS
+# 
+# A similar convention is followed on other DataSources for convience
+# but PyMca app does not depend on these being available.
+###############################################################################
 
-def test_refresh_method_exists():
-    """TiledDataSource has a callable refresh) method."""
+def test_refresh():
+    """TiledDataSource has a callable refresh method."""
     source = TiledDataSource()
     source.refresh()
+
+
+@pytest.mark.xfail(reason="Method not yet needed")
+def test_getSourceInfo():
+    """TiledDataSource has a callable getSourceInfo method."""
+    source = TiledDataSource()
+    source.getSourceInfo()
+
+
+@pytest.mark.xfail(reason="Method not yet needed")
+def test_getSourceInfo_values():
+    """TiledDataSource.getSourceInfo method returns a valid mapping."""
+    source = TiledDataSource()
+    info = source.getSourceInfo()
+    # These fields are generally defined by other DataSources
+    info["SourceName"]
+    info["SourceType"]
+    info["KeyList"]
+    info["Size"]
+
+
+@pytest.mark.xfail(reason="Method not yet needed")
+def test_getKeyInfo():
+    """TiledDataSource has a callable test_getKeyInfo method."""
+    source = TiledDataSource()
+    source.getKeyInfo("This key value is not used by this test")
+
+
+@pytest.mark.xfail(reason="Method not yet needed")
+def test_getKeyInfo_values():
+    """TiledDataSource.getKeyInfo method returns a valid mapping."""
+    source = TiledDataSource()
+    info = source.getKeyInfo("This key value is not used by this test")
+    # These fields are generally defined by other DataSources
+    info["SourceName"]
+    info["SourceType"]
+    info["Key"]
