ac3ee9e5849ef38d738c0d20a4b255f3086e1655
Abigail Giles <agiles@bnl.gov>
2025-07-23 15:10:03 -0400

    Cleanup tiled threading worker

--- PyMca5/PyMcaGui/io/TiledWorker.py
+++ PyMca5/PyMcaGui/io/TiledWorker.py
@@ -7,24 +7,37 @@ class TiledWorkerSignals(QObject):
 
 
 class TiledWorker(QRunnable):
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        *,
+        client,
+        current_page,
+        node_path_parts,
+        rows_per_page,
+        search_results,
+        **kwargs,
+    ):
         super().__init__()
         self.signals = TiledWorkerSignals()
-        self.rows_per_page = kwargs["rows_per_page"]
-        self.current_page = kwargs["current_page"]
-        self.client = kwargs["client"]
-        self.search_results = kwargs["search_results"]
-        self.node_path_parts = kwargs["node_path_parts"]
+        self.rows_per_page = rows_per_page
+        self.current_page = current_page
+        self.client = client
+        self.search_results = search_results
+        self.node_path_parts = node_path_parts
 
     def run(self):
         if self.search_results is None:
             catalog_or_search_results = self.client
         else:
             catalog_or_search_results = self.search_results
+
         node_offset = self.rows_per_page * self.current_page
+        selection = slice(node_offset, node_offset + self.rows_per_page)
+
         if self.node_path_parts:
-            results = catalog_or_search_results[self.node_path_parts[0]].items()[node_offset: node_offset + self.rows_per_page]
+            results = catalog_or_search_results[self.node_path_parts[0]].items()[selection]
         else:
-            results = catalog_or_search_results.items()[node_offset: node_offset + self.rows_per_page]
+            results = catalog_or_search_results.items()[selection]
+
         self.signals.finished.emit()
         self.signals.results.emit(results)
